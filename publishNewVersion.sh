4ec6bd40e0d6c6d7af2f6889db225dee32a2199b
Daniel Fischer <daniel@d-fischer.dev>
2020-03-12 16:30:12 +0100

    update build script

--- publishNewVersion.sh
+++ publishNewVersion.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+
+set -e
+
 CWD="$(pwd)"
 cd "$(dirname $0)"
+
+yarn lint
+yarn prettier:check
+yarn build
+
 VERSIONTYPE="${1:-patch}"
 npm version --preid pre ${VERSIONTYPE} -m "release version %s"
 case ${VERSIONTYPE} in
